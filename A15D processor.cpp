3cab870e6c73c522b80da069a9a3a1d9b298c95f
Grodekkk <grodek7483@gmail.com>
2025-04-05 23:35:30 +0200

    '`' and '&' commands added
    not 100% correct but it's a good start

--- A15D processor.cpp
+++ A15D processor.cpp
@@ -7,21 +7,62 @@ char program_mem[20000];
 char *mem_ptr = program_mem;
 int ptr_value = 0;
 
+struct ListElement;
+
+//==================================== LIST DATA & FUNCTIONS==================================================================
+struct List
+{
+    ListElement* startOfList;           //pointer to 1st position on the list
+};
+
+struct ListElement
+{
+    char value;
+    ListElement* nextListItem;
+};
+
+
+
+
 //===================================== STACK DATA & FUNCTIONS ============================================================================
 //this struct is a single stack element
+    
+int stackCounter = 0;                                   //shows current elements on stack, no elements and one elements count as zero need to be aware of that
 struct StackField 
 {
-    int value;
+    List value;
     StackField* previous_element;                       //pointer to a stack element "bellow" this element
 };
 
 StackField* stack_ptr = nullptr;                        //points to top of a stack
                                                         //at the beggining no stack -> points nowhere
 
-void push(int value)                                    //*& means the adress of pointer will change
+
+//append value at beggining of the list
+void appendOnList(char value)
+{
+    //case with empty list
+    if (stack_ptr->value.startOfList == nullptr)
+    {
+        stack_ptr->value.startOfList = new ListElement;
+        stack_ptr->value.startOfList->value = value;
+        stack_ptr->value.startOfList->nextListItem = nullptr;
+    }
+    else
+    {
+        //every other case, we make new beggining of the list, no need for switching every item
+        ListElement* newListElement = new ListElement;
+        newListElement->value = value;
+        newListElement->nextListItem = stack_ptr->value.startOfList;
+        stack_ptr->value.startOfList = newListElement;
+    }
+}
+
+//need fixing with lists, checking if works correctly
+void push(List* newList)
 {
     StackField* newField = new StackField;              //we create new stackfield and name it "newField" [TEMPORALY]
-    newField->value = value;                            
+    newField->value = *newList;//!!!  [might be incorrect]                          
     if (stack_ptr == nullptr)
     {
         newField->previous_element = nullptr;           //first element of stack doesnt have any element below
@@ -31,14 +72,57 @@ void push(int value)                                    //*& means the adress of
         newField->previous_element = stack_ptr;
     }
     stack_ptr = newField;
+    stackCounter++;
 }
 
-void show()
+void printList(ListElement* listIterator)
 {
-    cout << stack_ptr->value << "<-- top stack value]]" << endl;
+    if (listIterator->nextListItem == nullptr)
+    {
+        cout << listIterator->value << endl;        //prints last value and end a function at the end of the list
+        return;
+    }
+    else
+    {
+        cout << listIterator->value;                //prints current element of the list
+        listIterator = listIterator->nextListItem;  //listIteratr jumps to next element of list
+        printList(listIterator);
+    }
+
 }
 
-//==================================== LIST DATA & FUNCTIONS==================================================================
+//this will be '&' function, print entire content of stack.
+void show(StackField* currentStackField)
+{
+    //cout << stack_ptr->value << "<-- top stack value]]" << endl;
+    if (stack_ptr == nullptr)
+    {
+        cout << "emptystack" << endl;
+        return;
+    }
+    //todo number od the stack
+
+    //this propably cannot be here
+    
+
+    //is it correct place for this???
+    ListElement* listIterator = currentStackField->value.startOfList; //list iterator start at beggining of list at current stack level
+    if (currentStackField->previous_element == nullptr)
+    {
+        printList(listIterator);            //print list on this stack field
+        return;
+    }
+    else
+    {
+        printList(listIterator);                                        //print current list
+        currentStackField = currentStackField->previous_element;        //change pointer to one layer lower
+        show(currentStackField);                                        //do it once again until there is nothing under
+
+    }
+    //everything works by far, we just dont have the "recursion here" 
+    
+}
+
 
 
 
@@ -60,20 +144,43 @@ int main()
     {
         switch(mem_ptr[ptr_value])
         {
-        case '.':
-            int tempNotList;
-            cin >> tempNotList;
-            push(tempNotList);
-            ptr_value++;
-            break;
-        case '$':
-            show();
-            ptr_value++;
-            break;
-        case '%':
-            cout << ptr_value << " ==procent operation== " << endl;
-            ptr_value++;
-            break;
+        
+            case '`'://push empty list on the stack
+                {
+                    List* newList = new List;
+                    newList->startOfList = nullptr;         //list is empty so it points nowhere
+                    push(newList);
+                    ptr_value++;
+                    break;
+                }
+               
+        
+            case '&':
+                {
+                    StackField* currentStackField = stack_ptr;   //our inside pointer that will iterate thru all stack
+                    show(currentStackField);
+                    ptr_value++;
+                    break;
+                }
+                
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            case '-':
+                {
+                    appendOnList(mem_ptr[ptr_value]);
+                    ptr_value++;
+                    break;
+                }
+                
+
         }
             
 
