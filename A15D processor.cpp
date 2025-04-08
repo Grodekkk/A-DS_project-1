12261cef26d5abd1116b75846b147776740aa2d1
Grodekkk <grodek7483@gmail.com>
2025-04-09 01:02:22 +0200

    tests 1-6 passed, fixed memory leaks

--- A15D processor.cpp
+++ A15D processor.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <math.h>
-// test 1 63/90 testow
+// test 1 COMPLETE
+// test 2 COMPLETE
+// test 3 COMPLETE
+// test 4 COMPLETE
+// test 5 COMPLETE
+// test 6 COMPLETE
+// test 7 0/
 using namespace std;
 
 //==============TO DO LIST=================================================
@@ -18,7 +24,7 @@ using namespace std;
 
 //program memory and instruction pointer
 char program_mem[20000];
-char *mem_ptr = program_mem;
+char* mem_ptr = program_mem;
 int ptr_value = 0;
 
 struct ListElement;
@@ -40,17 +46,28 @@ struct ListElement
 
 //===================================== STACK DATA & FUNCTIONS ============================================================================
 //this struct is a single stack element
-    
+
 int stackCounter = -1;                                   //shows current elements on stack, no elements and one elements count as zero need to be aware of that
-struct StackField 
+struct StackField
 {
     List value;
     StackField* previous_element;                       //pointer to a stack element "bellow" this element
 };
 
 StackField* stack_ptr = nullptr;                        //points to top of a stack
-                                                        //at the beggining no stack -> points nowhere
+//at the beggining no stack -> points nowhere
 
+bool stackIsNull()
+{
+    if (stack_ptr == nullptr)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
 //append value at beggining of the list
 void appendOnList(char value)
@@ -72,15 +89,14 @@ void appendOnList(char value)
     }
 }
 
-//need fixing with lists, checking if works correctly
+//add new empty list on the stack
 void push()
 {
-    List* newList = new List;
-    newList->startOfList = nullptr;         //list is empty so it points nowhere
+    //create new StackField
+    StackField* newField = new StackField;  //List is part of StackField, so it's also allocated here
+    newField->value.startOfList = nullptr;  //so we can do it like that
 
-    StackField* newField = new StackField;              //we create new stackfield and name it "newField" [TEMPORALY]
-    newField->value = *newList;//!!!  [might be incorrect] "shallow copy" ask abt it
-
+    //check if stack is empty
     if (stack_ptr == nullptr)
     {
         newField->previous_element = nullptr;           //first element of stack doesnt have any element below
@@ -90,8 +106,8 @@ void push()
         newField->previous_element = stack_ptr;
     }
 
+    //attach stack pointer to new element and increment stack count
     stack_ptr = newField;
-    //newField->value.startOfList = nullptr; //its also in the main [code cleaning]
     stackCounter++;
 }
 
@@ -130,8 +146,8 @@ void show(StackField* currentStackField, int Counter)
     }
 
     //make pointer to start of the list at this stack field
-    ListElement* listIterator = currentStackField->value.startOfList; 
-    
+    ListElement* listIterator = currentStackField->value.startOfList;
+
     //end ofrecursion when we reached end of the stack
     if (currentStackField->previous_element == nullptr)
     {
@@ -147,16 +163,16 @@ void show(StackField* currentStackField, int Counter)
     }
     else
     {
-        
+
         //change pointer to one layer lower [recursion]
-        currentStackField = currentStackField->previous_element;        
-        show(currentStackField, (Counter+1));                                        
+        currentStackField = currentStackField->previous_element;
+        show(currentStackField, (Counter + 1));
 
         //printing empty list if list is empty
         if (listIterator == nullptr)
         {
             cout << Counter << ": ";
-            cout << endl;   
+            cout << endl;
         }
         else
         {
@@ -189,7 +205,7 @@ void copyStackTopHelper(ListElement* startOfList, ListElement* startOfStackList)
 
     //copy contents of existing list field
     startOfList->value = startOfStackList->value; ///[!!!] maybe before if
-    
+
     //make new list points to new list element -> instead of copyint pointer to old list
     startOfList->nextListItem = new ListElement;
 
@@ -214,6 +230,12 @@ bool isStackTopEmptyList()
 //put a exact copy of list from top on top of an stack
 void copyStackTop()
 {
+    //check if stacktop is empty
+    if (stack_ptr == nullptr)
+    {
+        return;         
+    }
+
     //check if StackTop is an emptylist
     if (isStackTopEmptyList())
     {
@@ -221,24 +243,20 @@ void copyStackTop()
         return;
     }
 
-
-    //create new list
-    List* newList = new List;
-    newList->startOfList = new ListElement; //this is usually null previously ***new ListElement*** OR ***nullptr*** but without values so it's trash memory
-
-    //create new stack field
+    //create new StackField, list with one emptyElement, once again List is already initiated here
     StackField* newField = new StackField;
-    newField->value = *newList;
-
+    newField->value.startOfList = new ListElement; //need to decide between new ListElement/nullptr
+    newField->value.startOfList->nextListItem = nullptr;
     //getting pointers of our stackfield, NULL if on bottom previous stackfield if not
-    if (stack_ptr == nullptr)
-    {
-        newField->previous_element = nullptr;           //first element of stack doesnt have any element below
-    }
-    else
-    {
+   //THIS IF STATEMENT 99% CHANCE DOES NOTHING, FUNCTION ENDS IF NULLPTR
+   if (stack_ptr == nullptr)
+   {
+       newField->previous_element = nullptr;           //first element of stack doesnt have any element below
+   }
+   else
+   {
         newField->previous_element = stack_ptr;
-    }
+   }
 
     //if copied list is empty, do not copy it
     if (stack_ptr->value.startOfList == nullptr)
@@ -247,12 +265,12 @@ void copyStackTop()
     }
     else
     {
-        ListElement* startOfList = newList->startOfList;
-        ListElement* startOfStackList = stack_ptr->value.startOfList; 
+        ListElement* startOfList = newField->value.startOfList;
+        ListElement* startOfStackList = stack_ptr->value.startOfList;
         copyStackTopHelper(startOfList, startOfStackList);
     }
     //list copying mechanic, maybe another function with recursion
-    
+
     //after everything move stack pointer to the new field
 
     stackCounter++;
@@ -262,6 +280,11 @@ void copyStackTop()
 //switch top and below lists in places
 void switchLists()
 {
+    //error handling --> this can be made into function
+    if (stackIsNull())
+    {
+        return;
+    }
     //get pointer to previous element of a bellow field
     StackField* bellowFieldPointer = stack_ptr->previous_element->previous_element;
 
@@ -276,11 +299,6 @@ void switchLists()
     bellowField->previous_element = topField;
 
     stack_ptr = bellowField;
-
-
-
-
-
 }
 
 //remove item from stack [helper function]
@@ -318,14 +336,14 @@ void pop()
     {
         stack_ptr = stack_ptr->previous_element;
     }
-    
+
     //if list is not empty, delete it
     if (cleaner->value.startOfList != nullptr)
     {
         ListElement* listCleaner = cleaner->value.startOfList;
         deleteStackContent(listCleaner);
     }
-    
+
     delete cleaner;
     stackCounter--;
 }
@@ -338,7 +356,7 @@ int getListCount(ListElement* listSearched, int listSize)
     {
         return listSize;
     }
-                          //also dlaczego nie dziala tutaj listSize++, a (listSize + 1) już tak   
+    //also dlaczego nie dziala tutaj listSize++, a (listSize + 1) już tak   
     return getListCount(listSearched->nextListItem, (listSize + 1));
 }
 
@@ -359,13 +377,13 @@ int sumUpList(ListElement* listSearched, int powerOfTen, int returner)
         return returner;
     }
 
-    return sumUpList(listSearched->nextListItem, (powerOfTen +1), (returner));
+    return sumUpList(listSearched->nextListItem, (powerOfTen + 1), (returner));
 }
 
 //returns contents of a list as an integer [main]
 int readListToInt()
 {
-    
+
     //int listSize = getListCount(stack_ptr->value.startOfList, 1); //not used because of misinterpretation of list
     int numValue = sumUpList(stack_ptr->value.startOfList, 0, 0);
     return numValue;
@@ -390,16 +408,12 @@ void popAndSwitch()
     //remove that list/number from stack
     pop();
 
-    //copy the number-ish pos of stack to the top
-    
-    
-    //create new list
-    List* newList = new List;
-    newList->startOfList = new ListElement;
+    //create new stack field, again List is part of StackField -> also initiated
+    StackField* newField = new StackField;
+    newField->value.startOfList = new ListElement;
 
-    //create new stack field
-    StackField* newField = new StackField;              
-    newField->value = *newList;
+    //start of list points to nowhere
+    newField->value.startOfList->nextListItem = nullptr;
 
     //append new stackfield to the stack
     if (stack_ptr == nullptr)
@@ -416,11 +430,11 @@ void popAndSwitch()
 
     //FIX START
     //list copying 
-    ListElement* startOfList = newList->startOfList;                    //this is correct
+    ListElement* startOfList = newField->value.startOfList;                    //this is correct
     // replace stack_ptr->value.startOfList to something else
     ListElement* startOfStackList = numPosition->value.startOfList;       //this need to be our i-sh field
     copyStackTopHelper(startOfList, startOfStackList);
-    
+
     //FIX STOP
 
 
@@ -449,88 +463,114 @@ void readStackChars(ListElement* listSearched)
 
 int main()
 {
-//loading all program instructions
+    //loading all program instructions
     cin >> program_mem;
-    
-//iterating through all program instructions
+
+    //iterating through all program instructions
     while (mem_ptr[ptr_value] != '\0')
     {
-        switch(mem_ptr[ptr_value])
+        switch (mem_ptr[ptr_value])
         {
-        
-            case '\''://push empty list on the stack
-                {  
-                    push();
-                    ptr_value++;
-                    break;
-                }
-               
-            case '&':
-                {
-                    int stackCpy = 0; //!!! zamiana z stackCounter
-                    StackField* currentStackField = stack_ptr;   //our inside pointer that will iterate thru all stack
-                    show(currentStackField, stackCpy);
-                    ptr_value++;
-                    break;
-                }
 
-            case ':':
-            {
-                copyStackTop();
+        case '\''://push empty list on the stack
+        {
+            push();
+            ptr_value++;
+            break;
+        }
 
-                ptr_value++;
-                break;
-            }
+        case '&':
+        {
+            int stackCpy = 0; //!!! zamiana z stackCounter
+            StackField* currentStackField = stack_ptr;   //our inside pointer that will iterate thru all stack
+            show(currentStackField, stackCpy);
+            ptr_value++;
+            break;
+        }
 
-            case ';':
-            {
-                switchLists();
-                ptr_value++;
-                break;
-            }
+        case ':':
+        {
+            copyStackTop();
 
-            case ',':
-            {
-                pop();
-                ptr_value++;
-                break;
-            }
+            ptr_value++;
+            break;
+        }
 
-            case '@':
-            {
+        case ';':
+        {
+            switchLists();
+            ptr_value++;
+            break;
+        }
 
-                popAndSwitch();
-                /*
-                //test dlugosci listy
-                cout << "liczba na stosie [INT]: " << readListToInt() << endl;
-                cout << "liczba na stosie [CHAR]: ";
-                readStackChars(stack_ptr->value.startOfList);
-                cout << endl;
-                */
-                ptr_value++;
-                break;
-            }
-                
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
-            case '9':
-            case '-':
-                {
-                    appendOnList(mem_ptr[ptr_value]);
-                    ptr_value++;
-                    break;
-                }
-            default:
-            {
-                ptr_value++;
-            }
+        case ',':
+        {
+            pop();
+            ptr_value++;
+            break;
+        }
+
+        case '@':
+        {
+
+            popAndSwitch();
+            /*
+            //test dlugosci listy
+            cout << "liczba na stosie [INT]: " << readListToInt() << endl;
+            cout << "liczba na stosie [CHAR]: ";
+            readStackChars(stack_ptr->value.startOfList);
+            cout << endl;
+            */
+            ptr_value++;
+            break;
+        }
+        //check if this can be moved to "default"
+        case 'q':
+        case 'w':
+        case 'e':
+        case 'r':
+        case 't':
+        case 'y':
+        case 'u':
+        case 'i':
+        case 'o':
+        case 'p':
+        case 'a':
+        case 's':
+        case 'd':
+        case 'f':
+        case 'g':
+        case 'h':
+        case 'j':
+        case 'k':
+        case 'l':
+        case 'z':
+        case 'x':
+        case 'c':
+        case 'v':
+        case 'b':
+        case 'n':
+        case 'm':
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case '-':
+        {
+            appendOnList(mem_ptr[ptr_value]);
+            ptr_value++;
+            break;
+        }
+        default:
+        {
+            ptr_value++;
+        }
         }
     }
 }
